ff7a5229b9d4bbcb5528d7f994b8ea68eb7bbcca
刘然 <liuranw@yonyou.com>
2022-01-05 18:57:25 +0800

    xiugai package name

--- lr-server/lr-web/src/main/java/org/com/lr/controller/tools/ToolsPdfController.java
+++ lr-server/lr-web/src/main/java/org/com/lr/controller/tools/ToolsPdfController.java
@@ -3,6 +3,7 @@ package org.com.lr.controller.tools;
 import org.com.lr.model.RespBean;
 import org.com.lr.service.service.ToolsPdfService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,7 +14,7 @@ public class ToolsPdfController {
 
   @Autowired
    private ToolsPdfService toolsPdfService;
-  @RequestMapping("/upload")
+  @PostMapping("/upload")
   public RespBean uploadPdf(MultipartFile file) {
     return toolsPdfService.uploadPdf(file);
   }
